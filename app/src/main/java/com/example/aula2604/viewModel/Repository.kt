64c01186f96dabf830c059b595ffc22711768bf1
Baneza-LCM <baneza68liv@gmail.com>
2024-05-24 17:41:19 -0300

    24/05/2024

---
+++ app/src/main/java/com/example/aula2604/viewModel/Repository.kt
@@ -0,0 +1,16 @@
+package com.example.aula2604.viewModel
+
+import com.example.aula2604.roomDB.Pessoa
+import com.example.aula2604.roomDB.PessoaDataBase
+
+class Repository(private val db: PessoaDataBase) {
+    suspend fun upsertPessoa(pessoa: Pessoa){
+        db.pessoaDao().upsertPessoa(pessoa)
+    }
+
+    suspend fun deletePessoa(pessoa: Pessoa){
+        db.pessoaDao().deletePessoa(pessoa)
+    }
+
+    fun getAllPessoa() = db.pessoaDao().getAllPessoa()
+}
